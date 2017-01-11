ba5754a1722bd24ef32e2b1b8c0f0ebdca5a308f
ChrisRackauckas <me@chrisrackauckas.com>
2017-01-11 10:51:54 -0800

    add probints

--- src/DiffEqUncertainty.jl
+++ src/DiffEqUncertainty.jl
@@ -1,5 +1,22 @@
 module DiffEqUncertainty
 
-# package code goes here
+using DiffEqBase
 
-end # module
+immutable ProbIntsCache{T}
+  σ::T
+  order::Int
+end
+function (p::ProbIntsCache)(integrator)
+  integrator.u .= integrator.u .+ p.σ*sqrt(integrator.dt^(2*p.order))*randn(size(integrator.u))
+end
+
+function ProbIntsUncertainty(σ,order,save=true)
+  affect! = ProbIntsCache(σ,order)
+  condtion = (t,u,integrator) -> true
+  save_positions = (save,false)
+  DiscreteCallback(condtion,affect!,save_positions)
+end
+
+export ProbIntsUncertainty
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,18 @@
-using DiffEqUncertainty
+using DiffEqUncertainty, DiffEqBase, OrdinaryDiffEq, DiffEqProblemLibrary, DiffEqMonteCarlo
 using Base.Test
+
+using ParameterizedFunctions
+g = @ode_def_bare LorenzExample begin
+  dx = σ*(y-x)
+  dy = x*(ρ-z) - y
+  dz = x*y - β*z
+end σ=>10.0 ρ=>28.0 β=(8/3)
+u0 = [1.0;0.0;0.0]
+tspan = (0.0,10.0)
+prob = ODEProblem(g,u0,tspan)
+
+cb = ProbIntsUncertainty(1e4,5)
+solve(prob,Tsit5())
+sim = monte_carlo_simulation(prob,Tsit5(),num_monte=10,callback=cb,adaptive=false,dt=1/10)
+
+using Plots; plot(sim,vars=(0,1),linealpha=0.4)
