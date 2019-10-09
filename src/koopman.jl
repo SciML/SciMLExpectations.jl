9164b1049f187b26d1b8243fd806f4701fb2a654
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-10-09 17:50:50 -0400

    Ensemble interface

--- src/koopman.jl
+++ src/koopman.jl
@@ -2,10 +2,11 @@ function koopman(g,prob,u0,p,args...;kwargs...)
     g(solve(remake(prob,u0=u0,p=p),args...;kwargs...))
 end
 
-function koopman_cost(u0s,ps,g,prob,args...;maxevals=0,
+function koopman_cost(u0s,ps,g,prob,args...;maxiters=0,
+                      intalg = HCubatureJL(),
                       ireltol = 1e-2, iabstol=1e-2,kwargs...)
   n = length(u0s)
-  function _f(x)
+  function _f(x,p)
     u0 = x[1:n]
     p  = x[n+1:end]
     k = koopman(g,prob,u0,p,args...;kwargs...)
@@ -14,8 +15,9 @@ function koopman_cost(u0s,ps,g,prob,args...;maxevals=0,
     k*w
   end
   xs = [u0s;ps]
-  hcubature(_f, minimum.(xs), maximum.(xs);
-            reltol=ireltol, abstol=iabstol, maxevals = maxevals)
+  intprob = QuadratureProblem(_f,minimum.(xs),maximum.(xs))
+  sol = solve(intprob,intalg,reltol=ireltol,
+              abstol=iabstol,maxiters = maxiters)
 end
 
 function montecarlo_cost(u0s,ps,g,prob,args...;trajectories,kwargs...)
