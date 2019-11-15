2a7c166b4218a11f30cfb4da751e9cb9e15cddae
Adam Gerlach <adam.gerlach.1@afresearchlab.com>
2019-11-15 20:08:51 +0000

    working prototype of interface mixing numbers and distributions for IC and params

--- src/koopman.jl
+++ src/koopman.jl
@@ -1,7 +1,8 @@
 function make_S(prob,args...;kwargs...)
+  n_states = length(prob.u0)
     function s_(ext_state)
-        u0s = ext_state[1:length(prob.u0)]
-        ps = ext_state[(length(u0) + 1):end]
+        u0s = ext_state[1:n_states]
+        ps = ext_state[(n_states + 1):end]
         solve(remake(prob, u0 = u0s, p = ps), args...; kwargs...)
     end
 end
@@ -10,17 +11,20 @@ function koopman(g,prob,u0,p,args...;kwargs...)
     g(solve(remake(prob,u0=u0,p=p),args...;kwargs...))
 end
 
-function expectation(g,u0s, ps,prob,args...; maxiters=0, batch = 0, quadalg = HCubatureJL(),ireltol = 1e-2, iabstol = 1e-2, kwargs...)
-      a=3
+function expectation(g,u0s, ps,prob,args...;  kwargs...)
+
+    # find indices corresponding to distributions
     u0s_distIDX = isa.(u0s,Sampleable)
     ps_distIDX = isa.(ps,Sampleable)
 
+    # Store index/distribution pairs
     u0s_dist_pairs = [Pair(idx, u0s[idx]) for idx ∈ (1:length(u0s))[u0s_distIDX]]
     ps_dist_pairs = [Pair(idx, ps[idx]) for idx ∈ (1:length(ps))[ps_distIDX]]
 
+    # Initialize IC and param vectors for numbers, set values from any non-distribution elements
     u0s_num = zeros(length(u0s))
     ps_num = zeros(length(ps))
-# expectation(g,prob,u0s_num, ps_num, u0s_dist_pairs, ps_dist_pairs, args...; kwargs...)
+
     @. u0s_num[!u0s_distIDX] = u0s[!u0s_distIDX]
     @. ps_num[!ps_distIDX] = ps[!ps_distIDX]
 
@@ -34,32 +38,40 @@ function __expectation(g,u0s,ps,prob,
                     quadalg = HCubatureJL(),
                     ireltol = 1e-2, iabstol = 1e-2, kwargs...)
 
+    # verify number of random IC/params
     n_rand_u0s = length(u0s_dist_pairs)
     n_rand_ps  = length(ps_dist_pairs)
 
     @assert n_rand_ps <= length(ps)
     @assert n_rand_u0s <= length(u0s)
 
+    # unpack pairs
     stateIdx = getfield.(u0s_dist_pairs, :first)
     stateDist = getfield.(u0s_dist_pairs, :second)
 
     paramIdx = getfield.(ps_dist_pairs, :first)
     paramDist = getfield.(ps_dist_pairs, :second)
 
-    indxs = vcat(stateIdx, paramIdx .+ length(u0s))
-    dists = vcat(stateDist, paramDist)
+    # Create extended state-space as [state-space; params]
+    indxs = vcat(stateIdx, paramIdx .+ length(u0s))   # combine indices for random states/params
+    dists = vcat(stateDist, paramDist)                # combine distributions
 
+    # extract bounds for random variables of extended state-space
     minBounds = minimum.(dists)
     maxBounds = maximum.(dists)
 
-    ext_state = vcat(u0s, ps)  ## Type issues. Needs to not include distirubtion types
+    # create extended state space
+    ext_state = vcat(u0s, ps)
 
+    # Make mapping for extended state-space.
     S = make_S(prob, args...; kwargs...)
 
-    function _f(x)
-        ext_state[indxs] = x
-        prob = prod(pdf(a, b) for (a, b) in zip(dists, x))
-        return g(S(ext_state)) * prob
+    # Define the integrand for expectation
+    function _f(x, p)
+        ext_state[indxs] = x        # set values for indices corresponding to random variables
+        w = prod(pdf(a, b) for (a, b) in zip(dists, x))
+        k = g(S(ext_state))
+        return k*w
     end
 
     intprob = QuadratureProblem(_f,minBounds,maxBounds,batch=batch)
--- test/koopman.jl
+++ test/koopman.jl
@@ -2,7 +2,6 @@ using OrdinaryDiffEq, Distributions,
       DiffEqUncertainty, Test, Quadrature, Cubature
 
 
-
 function f(du,u,p,t)
   @inbounds begin
     du[1] = dx = p[1]*u[1] - u[1]*u[2]
@@ -19,7 +18,7 @@ sol = solve(remake(prob,u0=u0),Tsit5())
 cost(sol) = sum(max(x[1]-6,0) for x in sol.u)
 u0s = [Uniform(0.25,5.5),Uniform(0.25,5.5)]
 ps  = [Uniform(0.5,2.0)]
-@time sol = koopman_expectation(cost,u0s,ps,prob,Tsit5();iabstol=1e-3,ireltol=1e-3,maxiters=1000,saveat=0.1)
+sol = koopman_expectation(cost,u0s,ps,prob,Tsit5();iabstol=1e-3,ireltol=1e-3,maxiters=1000,saveat=0.1)
 c1, e1 = sol.u, sol.resid
 @time sol = koopman_expectation(cost,u0s,ps,prob,Tsit5(),EnsembleThreads();quadalg=CubatureJLh(),
                          batch=1000,iabstol=1e-3,ireltol=1e-3,
@@ -31,20 +30,57 @@ c2, e2 = sol.u, sol.resid
 @test abs(c1 - c3) < 0.1
 
 ##########
-u0s = [Uniform(0.25,5.5),Uniform(0.25,5.5)]
-ps  = [Uniform(0.5,2.0)]
-sol = expectation(cost,u0s,ps,prob,Tsit5();iabstol=1e-3,ireltol=1e-3,maxiters=1000,saveat=0.1)
-c4, e1 = sol.u, sol.resid
-@test c4 ≈ c1
+## Compare for all states/params random
+begin
+  u0 = [1.0;1.0]
+  tspan = (0.0,10.0)
+  p = [1.5]
+  prob = ODEProblem(f,u0,tspan,p)
+  u0s = [Uniform(0.25,5.5),Uniform(0.25,5.5)]
+  ps  = [Uniform(0.5,2.0)]
+  sol = expectation(cost,u0s,ps,prob,Tsit5();iabstol=1e-3,ireltol=1e-3,maxiters=1000,saveat=0.1)
+  c4, e4 = sol.u, sol.resid
+  @test c4 ≈ c1
+end
 
-#=
-using DiffEqGPU
-@time sol = koopman_expectation(cost,u0s,ps,prob,Tsit5(),EnsembleGPUArray();quadalg=CubatureJLh(),
-                         batch=1000,iabstol=1e-3,ireltol=1e-3,
-                         maxiters=2000,saveat=0.1)
-c2, e2 = sol.u, sol.resid
-@test abs(c1 - c2) < 0.1
+## compare for subset of states random
+begin
+  u0 = [1.0;1.0]
+  tspan = (0.0,10.0)
+  p = [1.5]
+  prob = ODEProblem(f,u0,tspan,p)
 
-@time c3 = montecarlo_expectation(cost,u0s,ps,prob,Tsit5(),EnsembleGPUArray();trajectories=100000,saveat=0.1)
-@test abs(c1 - c3) < 0.1
-=#
+  u0s = [Uniform(0.25,5.5),Uniform( (1.0 .+ [-1e-8, 1e-8])...)]   # Old interface hacked by using very tight distribution for 2nd state
+  ps  = [Uniform(0.5,2.0)]
+  sol = koopman_expectation(cost,u0s,ps,prob,Tsit5();iabstol=1e-6,ireltol=1e-6,maxiters=1000,saveat=0.1)
+  c5, e5 = sol.u, sol.resid
+
+  u0s = [Uniform(0.25,5.5), 1.0]
+  ps  = [Uniform(0.5,2.0)]
+  prob = ODEProblem(f,[1.0,1.0],tspan,p)
+  sol = expectation(cost,u0s,ps,prob,Tsit5();iabstol=1e-6,ireltol=1e-6,maxiters=1000,saveat=0.1)
+  c6, e6 = sol.u, sol.resid
+
+  @test abs(c6 - c5) < 0.02
+end
+
+## Compare with only random states
+begin
+  u0 = [1.0;1.0]
+  tspan = (0.0,10.0)
+  p = [1.5]
+  prob = ODEProblem(f,u0,tspan,p)
+
+  u0s = [Uniform(0.25,5.5),Uniform(0.25,5.5)]
+  ps  = [Uniform((1.5 .+ [-1e-8, 1e-8])...)]  # Old interface hacked by using very tight distribution for 2nd state
+  sol = koopman_expectation(cost,u0s,ps,prob,Tsit5();iabstol=1e-6,ireltol=1e-6,maxiters=1000,saveat=0.1)
+  c7, e7 = sol.u, sol.resid
+
+  u0s = [Uniform(0.25,5.5), Uniform(0.25,5.5)]
+  ps  = [1.5]
+  prob = ODEProblem(f,[1.0,1.0],tspan,p)
+  sol = expectation(cost,u0s,ps,prob,Tsit5();iabstol=1e-6,ireltol=1e-6,maxiters=1000,saveat=0.1)
+  c8, e8 = sol.u, sol.resid
+
+  @test abs(c7 - c8) < 0.02
+end
