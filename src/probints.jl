5038a0fb4bb60a6ae1fcdc390170753f09b7e789
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2020-06-15 00:53:07 -0400

    add Probints citation

--- src/probints.jl
+++ src/probints.jl
@@ -6,6 +6,10 @@ function (p::ProbIntsCache)(integrator)
   integrator.u .= integrator.u .+ p.σ*sqrt(integrator.dt^(2*p.order))*randn(size(integrator.u))
 end
 
+"""
+Conrad P., Girolami M., Särkkä S., Stuart A., Zygalakis. K, Probability
+Measures for Numerical Solutions of Differential Equations, arXiv:1506.04592
+"""
 function ProbIntsUncertainty(σ,order,save=true)
   affect! = ProbIntsCache(σ,order)
   condtion = (t,u,integrator) -> true
