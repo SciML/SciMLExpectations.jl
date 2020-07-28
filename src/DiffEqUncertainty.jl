102d9c70b260c149d364a520bfb044054e314e83
adam.gerlach.1@us.af.mil <adam.gerlach.1@us.af.mil>
2020-07-28 20:46:15 +0000

    clean up

--- src/DiffEqUncertainty.jl
+++ src/DiffEqUncertainty.jl
@@ -6,8 +6,6 @@ include("probints.jl")
 include("koopman.jl")
 
 export ProbIntsUncertainty,AdaptiveProbIntsUncertainty
-export koopman_expectation, montecarlo_expectation
-export koopman_expectation2
 export expectation, centralmoment, Koopman, MonteCarlo
 
 end
