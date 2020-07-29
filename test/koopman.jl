89a07953ab8b7c2f1743d4add64b72e001312517
Adam R Gerlach <adam.gerlach.1@us.af.mil>
2020-07-28 21:35:28 -0400

    add expectation tests

--- test/koopman.jl
+++ test/koopman.jl
@@ -1,41 +1,161 @@
 using OrdinaryDiffEq, Distributions,
-      DiffEqUncertainty, Test, Quadrature, Cubature
+      DiffEqUncertainty, Test, Quadrature, Cubature, Cuba, 
+      FiniteDiff, Zygote, ForwardDiff, DiffEqGPU, DiffEqSensitivity
 
-function f(du,u,p,t)
+quadalgs = [HCubatureJL(), CubatureJLh(), CubatureJLp(), CubaSUAVE(), CubaDivonne(), CubaCuhre()]
+batchmode = [EnsembleSerial(), EnsembleThreads(), EnsembleCPUArray()]#, EnsembleGPUArray()]
+
+function eom!(du,u,p,t)
   @inbounds begin
-    du[1] = dx = p[1]*u[1] - u[1]*u[2]
-    du[2] = dy = -3*u[2] + u[1]*u[2]
+    du[1] = u[2]
+    du[2] = -p[1]*u[1] - p[2]*u[2]
   end
   nothing
 end
 
 u0 = [1.0;1.0]
-tspan = (0.0,10.0)
-p = [1.5]
-prob = ODEProblem(f,u0,tspan,p)
-sol = solve(remake(prob,u0=u0),Tsit5())
-cost(sol) = sum(max(x[1]-6,0) for x in sol.u)
-u0s = [Uniform(0.25,5.5),Uniform(0.25,5.5)]
-ps  = [Uniform(0.5,2.0)]
-@time sol = koopman_expectation(cost,u0s,ps,prob,ps,Tsit5();quadalg=HCubatureJL(),iabstol=1e-3,ireltol=1e-3,maxiters=1000,saveat=0.1)
-c1, e1 = sol.u, sol.resid
-@time sol = koopman_expectation(cost,u0s,ps,prob,ps, Tsit5(),EnsembleThreads();quadalg=CubatureJLh(),
-                         batch=1000,iabstol=1e-3,ireltol=1e-3,
-                         maxiters=2000,saveat=0.1)
-c2, e2 = sol.u, sol.resid
-@test abs(c1 - c2[1]) < 0.1
+tspan = (0.0,3.0)
+p = [1.0, 2.0]
+prob = ODEProblem(eom!,u0,tspan,p)
 
-@time c3 = montecarlo_expectation(cost,u0s,ps,prob,Tsit5(),EnsembleThreads();trajectories=100000,saveat=0.1)
-@test abs(c1 - c3) < 0.1
+A = [0.0 1.0; -p[1] -p[2]]
+u0s_dist = [Uniform(1,10), Uniform(2,6)]
 
-#=
-using DiffEqGPU
-@time sol = koopman_expectation(cost,u0s,ps,prob,Tsit5(),EnsembleGPUArray();quadalg=CubatureJLh(),
-                         batch=1000,iabstol=1e-3,ireltol=1e-3,
-                         maxiters=2000,saveat=0.1)
-c2, e2 = sol.u, sol.resid
-@test abs(c1 - c2) < 0.1
+@testset "Koopman Expectation, nout = 1" begin
+  g(sol) = sol[1,end]
+  analytical = (exp(A*tspan[end])*mean.(u0s_dist))[1]
+  
+  for alg ∈ quadalgs
+    @info "$alg"
+    @test expectation(g, prob, u0s_dist, p, Koopman(), Tsit5(); quadalg = alg)[1] ≈ analytical rtol=1e-2
+  end
+end
 
-@time c3 = montecarlo_expectation(cost,u0s,ps,prob,Tsit5(),EnsembleGPUArray();trajectories=100000,saveat=0.1)
-@test abs(c1 - c3) < 0.1
-=#
+@testset "Koopman Expectation, nout > 1" begin
+  g(sol) = sol[:,end]
+  analytical = (exp(A*tspan[end])*mean.(u0s_dist))
+  
+  for alg ∈ quadalgs
+    @info "$alg"
+    @test expectation(g, prob, u0s_dist, p, Koopman(), Tsit5(); quadalg = alg, nout =2) ≈ analytical rtol=1e-2
+  end
+end
+
+@testset "Koopman Expectation, nout = 1, batch" begin
+  g(sol) = sol[1,end]
+  analytical = (exp(A*tspan[end])*mean.(u0s_dist))[1]
+
+  for bmode ∈ batchmode
+    for alg ∈ quadalgs
+      if alg isa HCubatureJL
+        continue
+      end
+      @info "nout = 1, batch mode = $bmode, $alg"
+      @test expectation(g, prob, u0s_dist, p, Koopman(), Tsit5(), bmode; quadalg = alg, batch = 15)[1] ≈ analytical rtol=1e-2
+    end
+  end
+end
+
+@testset "Koopman Expectation, nout > 1, batch" begin
+  g(sol) = sol[:,end]
+  analytical = (exp(A*tspan[end])*mean.(u0s_dist))
+
+  for bmode ∈ batchmode
+    for alg ∈ quadalgs
+      if alg isa HCubatureJL
+        continue
+      end
+      @info "nout = 2, batch mode = $bmode, $alg"
+      res = expectation(g, prob, u0s_dist, p, Koopman(), Tsit5(), bmode; 
+                        quadalg = alg, batch = 15, nout=2)
+      @test res ≈ analytical rtol=1e-2
+    end
+  end
+end
+
+############## Koopman AD ###############
+
+@testset "Koopman Expectation AD" begin
+  g(sol) = sol[1,end]
+  loss(p, alg) = expectation(g, prob, u0s_dist, p, Koopman(), Tsit5(); quadalg = alg)[1]
+  dp1 = FiniteDiff.finite_difference_gradient(p->loss(p, HCubatureJL()),p)
+  for alg ∈ quadalgs
+    @info "$alg, ForwardDiff"
+    @test ForwardDiff.gradient(p->loss(p,alg),p) ≈ dp1 rtol=1e-2
+    @info "$alg, Zygote"
+    @test Zygote.gradient(p->loss(p,alg),p)[1] ≈ dp1 rtol=1e-2
+  end
+end
+
+@testset "Koopman Expectation AD, batch" begin
+  g(sol) = sol[1,end]
+  loss(p, alg, bmode) = expectation(g, prob, u0s_dist, p, Koopman(), Tsit5(), bmode; quadalg = alg, batch = 10)[1]
+  dp1 = FiniteDiff.finite_difference_gradient(p->loss(p, CubatureJLh(), EnsembleThreads()),p)
+  for bmode ∈ batchmode
+    for alg ∈ quadalgs
+      if alg isa HCubatureJL #no batch support
+        continue
+      end
+      @info "$bmode, $alg, ForwardDiff"
+      @test_broken ForwardDiff.gradient(p->loss(p,alg,bmode),p) ≈ dp1 rtol=1e-2
+      @info "$bmode, $alg, Zygote"
+      @test_broken  Zygote.gradient(p->loss(p,alg,bmode),p)[1] ≈ dp1 rtol=1e-2
+    end
+  end
+end
+
+########## Central Moment ##############
+
+function eom!(du,u,p,t)
+  @inbounds begin
+    du[1] = p[1]*u[1]
+  end
+  nothing
+end
+
+u0 = [1.0]
+tspan = (0.0,3.0)
+p = [-.3]
+prob = ODEProblem(eom!,u0,tspan,p)
+
+u0s_dist = [Uniform(1,10)]
+
+@testset "Koopman Central Moment" begin
+  g(sol) = sol[1,end]
+  analytical = [0.0, exp(2*p[1]*tspan[end])*var(u0s_dist[1]), 0.0]
+  
+  for alg ∈ quadalgs
+    if alg isa CubaDivonne || alg isa CubaCuhre  #requires 2D spatial integration
+      continue
+    end
+    r = centralmoment(3, g, prob, u0s_dist, p, Koopman(), Tsit5(); 
+                      ireltol = 1e-8, iabstol = 1e-8, quadalg = alg) 
+    if alg isa CubaSUAVE
+      @test_broken r ≈ analytical rtol=1e-2
+    else
+      @test r ≈ analytical rtol=1e-2
+    end
+  end
+end
+
+@testset "Koopman Central Moment, batch" begin
+  g(sol) = sol[1,end]
+  analytical = [0.0, exp(2*p[1]*tspan[end])*var(u0s_dist[1]), 0.0]
+  
+  for bmode ∈ batchmode
+    for alg ∈ quadalgs
+      if alg isa CubaDivonne || alg isa CubaCuhre || alg isa HCubatureJL #requires 2D spatial integration
+        continue
+      end
+      @info "batch mode = $bmode, alg = $alg"
+      r = centralmoment(3, g, prob, u0s_dist, p, Koopman(), Tsit5(), bmode; 
+                        ireltol = 1e-8, iabstol = 1e-8, quadalg = alg, batch=15) 
+      if alg isa CubaSUAVE
+        @test_broken r ≈ analytical rtol=1e-2
+      else
+        @test r ≈ analytical rtol=1e-2
+      end
+    end
+  end
+end
+
