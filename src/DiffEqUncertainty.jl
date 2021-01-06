4f8ddf80419eacfc6ce158b6d01448d38087a59b
Chris Rackauckas <accounts@chrisrackauckas.com>
2021-01-05 22:53:48 -0500

    support KernelDensity estimates

--- src/DiffEqUncertainty.jl
+++ src/DiffEqUncertainty.jl
@@ -2,8 +2,15 @@ module DiffEqUncertainty
 
 using DiffEqBase, Statistics, Distributions, Reexport
 @reexport using Quadrature
+using KernelDensity
+
 include("probints.jl")
 include("koopman.jl")
+
+# Type Piracy, should upstream
+Base.eltype(K::UnivariateKDE)  = eltype(K.density)
+Base.minimum(K::UnivariateKDE) = minimum(K.density)
+Base.maximum(K::UnivariateKDE) = maximum(K.density)
 
 export ProbIntsUncertainty,AdaptiveProbIntsUncertainty
 export expectation, centralmoment, Koopman, MonteCarlo
