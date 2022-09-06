6fade462b03ba7b539bd5096a272b70a2ca46f86
agerlach <599421+agerlach@users.noreply.github.com>
2022-09-06 15:34:38 +0000

    remove Distributions type piracy

--- src/SciMLExpectations.jl
+++ src/SciMLExpectations.jl
@@ -20,14 +20,6 @@ Base.minimum(K::UnivariateKDE) = minimum(K.x)
 Base.maximum(K::UnivariateKDE) = maximum(K.x)
 Base.extrema(K::UnivariateKDE) = minimum(K), maximum(K)
 
-Base.minimum(d::AbstractMvNormal) = fill(-Inf, length(d))
-Base.maximum(d::AbstractMvNormal) = fill(Inf, length(d))
-Base.extrema(d::AbstractMvNormal) = minimum(d), maximum(d)
-
-Base.minimum(d::Product) = minimum.(d.v)
-Base.maximum(d::Product) = maximum.(d.v)
-Base.extrema(d::Product) = minimum(d), maximum(d)
-
 export Koopman, MonteCarlo, PrefusedAD, PostfusedAD, NonfusedAD
 export GenericDistribution, SystemMap, ExpectationProblem, build_integrand
 
