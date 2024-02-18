c7d8d10c0915c82f2a95efccd69a530426b51d32
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-02-18 08:58:39 -0500

    Update src/expectation.jl
    Co-authored-by: Lorenzo Van Munoz <66997677+lxvm@users.noreply.github.com>

--- src/expectation.jl
+++ src/expectation.jl
@@ -223,7 +223,7 @@ end
 
 # Integrate function to test new Adjoints, will need to roll up to Integrals.jl
 function integrate(quadalg, adalg::AbstractExpectationADAlgorithm, f, lb::TB, ub::TB, p;
-                   nout = 1, batch = 0,
+                   nout = nothing, batch = nothing,
                    kwargs...) where {TB}
     #TODO check batch iip type stability w/ IntegralProblem{XXXX}
     batch = batch==0 ? nothing : batch
